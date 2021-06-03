cbbaeb9650ac7863ab1f50a961a8245e15569200
Artur Shaiekhov <arttu9994@gmail.com>
2021-06-03 19:13:46 +0300

    feat: Add basic annotations and application.yml

--- src/main/java/com/arttu/gateway/GatewayApplication.java
+++ src/main/java/com/arttu/gateway/GatewayApplication.java
@@ -2,7 +2,13 @@ package com.arttu.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
